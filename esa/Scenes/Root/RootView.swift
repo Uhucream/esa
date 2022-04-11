d01b69895239d5d2bdd31974b10bb6df568d7529
Uhucream <aborog1324@yahoo.co.jp>
2022-04-11 23:26:58 +0900

    feat: RootView 作成 & ContentView にRootView の呼び出し追加

---
+++ esa/Scenes/Root/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  esa
+//  
+//  Created by TakashiUshikoshi on 2022/04/11
+//  
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
