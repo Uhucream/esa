42dc0aa9d9fab9c83a06bba3611edeb67410eb60
Uhucream <aborog1324@yahoo.co.jp>
2022-04-11 19:39:53 +0900

    Initial Commit

---
+++ esa/esaApp.swift
@@ -0,0 +1,21 @@
+//
+//  esaApp.swift
+//  esa
+//  
+//  Created by TakashiUshikoshi on 2022/04/11
+//  
+//
+
+import SwiftUI
+
+@main
+struct esaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
